6d57d6370621b983f3902209bbc7bd045facd872
Mohammed Kharoda <kharodawalam@gmail.com>
2023-11-08 11:16:02 +0530

    added fonts and theme styles update

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="font-grotesk text-2xl text-black_color underline">
       Hello world!
     </h1>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
- content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        colors: {
+            primary_color: "#b9ff66",
+            black_color: "#191a23",
+            grey_color: "#f3f3f3"
+        },
+        fontFamily: {
+            grotesk:['Space Grotesk', 'sans-serif']
+        },
+        extend: {},
+    },
+    plugins: [],
 }
 
